62b025b98d45e7031b033547aaa6304dcbb9c721
karyna <karishka.spark@gmail.com>
2025-02-28 10:30:32 +0200

    edit task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const refs = {
+    form: document.querySelector('.form'),
+};
+refs.form.addEventListener('submit', e => {
+    e.preventDefault();
+    const delay = Number(refs.form.delay.value);
+    const state = refs.form.state.value;
+
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (state === "fulfilled") {
+            resolve (delay)
+        } else {
+            reject(delay)
+        }
+    }, delay)
+})
+
+promise.then(delay => {
+    iziToast.success({
+    title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+    position: 'topRight'
+    })
+})
+promise.catch((delay) => {
+    iziToast.error({
+    title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+    position: 'topRight'
+    })
+})
+})
